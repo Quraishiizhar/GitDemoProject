f6efbfb21cecfd41569e9b5a0e6b5ea59b7e7f4d
Izhar Quraishi <quraishiizhar03@gmail.com>
2025-12-25 12:57:00 +0300

    s

--- BookApplication/src/main/java/com/example/BookApplication.java
+++ BookApplication/src/main/java/com/example/BookApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //ddd
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BookApplication {
 
 	public static void main(String[] args) {
